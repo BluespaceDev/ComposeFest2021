63a491b430032de096fca38be5f2740b9c95ecaf
bluespace <bluespace.dev@gmail.com>
2021-11-17 23:39:46 +0900

    7. Create your custom layout Using the layout modifier

--- week 2-1-Layouts in JetpackCompose/MyLayout/app/src/main/java/com/example/mylayout/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/MyLayout/app/src/main/java/com/example/mylayout/MainActivity.kt
@@ -18,8 +18,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.mylayout.ui.theme.MyLayoutTheme
@@ -144,10 +148,44 @@ fun ScrollList() {
     }
 }
 
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
 @Preview
 @Composable
 fun LayoutsCodelabPreview() {
     MyLayoutTheme {
         LayoutsCodelab()
+    }
+}
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    MyLayoutTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    MyLayoutTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
     }
 }
