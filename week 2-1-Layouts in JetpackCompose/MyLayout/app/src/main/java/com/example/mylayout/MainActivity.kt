8af3e955bb83239cecfb2b61be4953d479d25200
bluespace <bluespace.dev@gmail.com>
2021-11-19 23:13:43 +0900

    7. Create your custom layout Implementing a basic Column

--- week 2-1-Layouts in JetpackCompose/MyLayout/app/src/main/java/com/example/mylayout/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/MyLayout/app/src/main/java/com/example/mylayout/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -62,8 +63,14 @@ fun LayoutsCodelab() {
 
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        ScrollList()
+//    Column(modifier = modifier) {
+//        ScrollList()
+//    }
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
     }
 }
 
@@ -165,6 +172,30 @@ fun Modifier.firstBaselineToTop(
         }
     }
 )
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+
+        var yPosition = 0
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+                yPosition += placeable.height
+            }
+        }
+    }
+}
 
 @Preview
 @Composable
