20c5153aa0c5e04f2d5ce677386dda1bc6c4e9b9
bluespace <bluespace.dev@gmail.com>
2021-11-28 12:55:48 +0900

    4. Define your theme Create a Shape

--- week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
+++ week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
@@ -2,8 +2,6 @@ package com.codelab.theming.ui.start.theme
 
 import androidx.compose.ui.graphics.Color
 
-class Color {
-    val Red700 = Color(0xffdd0d3c)
-    val Red800 = Color(0xffd00036)
-    val Red900 = Color(0xffc20029)
-}
+val Red700 = Color(0xffdd0d3c)
+val Red800 = Color(0xffd00036)
+val Red900 = Color(0xffc20029)
--- week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
+++ week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -8,23 +8,22 @@ import com.codelab.theming.ui.finish.theme.Red700
 import com.codelab.theming.ui.finish.theme.Red800
 import com.codelab.theming.ui.finish.theme.Red900
 
-class Theme {
-    private val LightColors = lightColors(
-        primary = Red700,
-        primaryVariant = Red900,
-        onPrimary = Color.White,
-        secondary = Red700,
-        secondaryVariant = Red900,
-        onSecondary = Color.White,
-        error = Red800
-    )
+private val LightColors = lightColors(
+    primary = Red700,
+    primaryVariant = Red900,
+    onPrimary = Color.White,
+    secondary = Red700,
+    secondaryVariant = Red900,
+    onSecondary = Color.White,
+    error = Red800
+)
 
-    @Composable
-    fun JetnewsTheme(content: @Composable () -> Unit) {
-        MaterialTheme(
-            colors = LightColors,
-            typography = JetnewsTypography,
-            content = content
-        )
-    }
+@Composable
+fun JetnewsTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colors = LightColors,
+        typography = JetnewsTypography,
+        shapes = JetnewsShapes,
+        content = content
+    )
 }
