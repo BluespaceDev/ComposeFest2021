30e1a86fdc51f1d536f5a3ad0798ab26015e5a32
bluespace <bluespace.dev@gmail.com>
2021-11-30 22:48:30 +0900

    7. Testing Navigation in Compose Write your first test

--- week 3-3-Jetpack Compose Navigation/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
@@ -1,9 +1,33 @@
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.compose.rally.RallyNavHost
+import org.junit.Assert.fail
+import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class RallyNavHostTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
+    lateinit var navController: NavHostController
+
+    @Before
+    fun setupRallyNavHost() {
+        composeTestRule.setContent {
+            navController = rememberNavController()
+            RallyNavHost(navController = navController)
+        }
+    }
+
+    @Test
+    fun rallyNavHost() {
+        composeTestRule
+            .onNodeWithContentDescription("Overview Screen")
+            .assertIsDisplayed()
+    }
 
 }
