2857705b6ffa67744ea0e5a4a0da9dd34311f53a
bluespace <bluespace.dev@gmail.com>
2021-12-01 12:10:54 +0900

    3. Create a simple UI test Testing in isolation

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,12 +1,20 @@
 package com.example.compose.rally
 
+import androidx.compose.material.Text
 import androidx.compose.ui.test.junit4.createComposeRule
+import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
+import org.junit.Test
 
 class TopAppBarTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
 
-
+    @Test
+    fun myTest() {
+        composeTestRule.setContent {
+            Text("You can set any Compose content!")
+        }
+    }
 }
